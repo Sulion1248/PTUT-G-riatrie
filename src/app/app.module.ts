86a71ccaab7a3914041f4d747554c874b33b8e4d
thibaut <thibaut.raven@gmail.com>
2019-11-14 19:01:38 +0100

    ajout routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { PatientComponent } from './patient/patient.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
+import { IdentificationComponent } from './identification/identification.component';
+import { ServiceComponent } from './service/service.component';
+import { ErreurComponent } from './erreur/erreur.component';
+import { AppRoutesModule } from './app-routes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
-  ],
+    ErreurComponent,
+    ServiceComponent,
+    IdentificationComponent,
+    PatientComponent  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
